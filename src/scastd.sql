014cd71f922d908aaf5782de0095b14ad2947cfb
David Andrew St John <davestj@gmail.com>
2025-08-09 09:07:21 -0700

    Modernize SQL dumps and add PostgreSQL schema

--- src/scastd.sql
+++ src/scastd.sql
@@ -14,7 +14,7 @@ CREATE TABLE scastd_memberinfo (
   password varchar(155) DEFAULT '' NOT NULL,
   gather_flag varchar(155) DEFAULT '' NOT NULL,
   PRIMARY KEY (serverURL)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Dumping data for table 'scastd_memberinfo'
@@ -28,9 +28,9 @@ INSERT INTO scastd_memberinfo VALUES ('http://boa.mediacast1.com:9908','party732
 
 DROP TABLE IF EXISTS scastd_runtime;
 CREATE TABLE scastd_runtime (
-  sleeptime int(11),
-  logfile varchar(255)
-);
+  sleeptime INT DEFAULT NULL,
+  logfile VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Dumping data for table 'scastd_runtime'
@@ -44,14 +44,14 @@ INSERT INTO scastd_runtime VALUES (30,'./scastd.log');
 
 DROP TABLE IF EXISTS scastd_serverinfo;
 CREATE TABLE scastd_serverinfo (
-  serverURL varchar(255),
-  currentlisteners int(11),
-  peaklisteners int(11),
-  maxlisteners int(11),
-  averagetime int(11),
-  streamhits int(11),
-  time timestamp(14)
-);
+  serverURL VARCHAR(255) DEFAULT NULL,
+  currentlisteners INT DEFAULT NULL,
+  peaklisteners INT DEFAULT NULL,
+  maxlisteners INT DEFAULT NULL,
+  averagetime INT DEFAULT NULL,
+  streamhits INT DEFAULT NULL,
+  time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Dumping data for table 'scastd_serverinfo'
@@ -64,10 +64,10 @@ CREATE TABLE scastd_serverinfo (
 
 DROP TABLE IF EXISTS scastd_songinfo;
 CREATE TABLE scastd_songinfo (
-  serverURL varchar(255),
-  songTitle varchar(255),
-  time timestamp(14)
-);
+  serverURL VARCHAR(255) DEFAULT NULL,
+  songTitle VARCHAR(255) DEFAULT NULL,
+  time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Dumping data for table 'scastd_songinfo'
@@ -80,29 +80,29 @@ CREATE TABLE scastd_songinfo (
 
 DROP TABLE IF EXISTS scastd_xml;
 CREATE TABLE scastd_xml (
-  id int(10) NOT NULL auto_increment,
-  currentlisteners varchar(155) DEFAULT '' NOT NULL,
-  peaklisteners varchar(155) DEFAULT '' NOT NULL,
-  maxlisteners varchar(155) DEFAULT '' NOT NULL,
-  reportedlisteners varchar(155) DEFAULT '' NOT NULL,
-  averagetime varchar(255) DEFAULT '' NOT NULL,
-  servergenre varchar(155) DEFAULT '' NOT NULL,
-  serverurl varchar(255) DEFAULT '' NOT NULL,
-  servertitle varchar(255) DEFAULT '' NOT NULL,
-  currentsong varchar(255) DEFAULT '' NOT NULL,
-  webhits varchar(155) DEFAULT '' NOT NULL,
-  streamhits varchar(255) DEFAULT '' NOT NULL,
-  hostname varchar(255) DEFAULT '' NOT NULL,
-  useragent varchar(255) DEFAULT '' NOT NULL,
-  connecttime varchar(155) DEFAULT '' NOT NULL,
-  songtitle varchar(255) DEFAULT '' NOT NULL,
-  playedat varchar(55) DEFAULT '' NOT NULL,
-  bwidthus varchar(55) DEFAULT '' NOT NULL,
-  totbwidthus varchar(255) DEFAULT '' NOT NULL,
-  date date DEFAULT '0000-00-00' NOT NULL,
-  time time DEFAULT '00:00:00' NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  currentlisteners VARCHAR(155) DEFAULT '' NOT NULL,
+  peaklisteners VARCHAR(155) DEFAULT '' NOT NULL,
+  maxlisteners VARCHAR(155) DEFAULT '' NOT NULL,
+  reportedlisteners VARCHAR(155) DEFAULT '' NOT NULL,
+  averagetime VARCHAR(255) DEFAULT '' NOT NULL,
+  servergenre VARCHAR(155) DEFAULT '' NOT NULL,
+  serverurl VARCHAR(255) DEFAULT '' NOT NULL,
+  servertitle VARCHAR(255) DEFAULT '' NOT NULL,
+  currentsong VARCHAR(255) DEFAULT '' NOT NULL,
+  webhits VARCHAR(155) DEFAULT '' NOT NULL,
+  streamhits VARCHAR(255) DEFAULT '' NOT NULL,
+  hostname VARCHAR(255) DEFAULT '' NOT NULL,
+  useragent VARCHAR(255) DEFAULT '' NOT NULL,
+  connecttime VARCHAR(155) DEFAULT '' NOT NULL,
+  songtitle VARCHAR(255) DEFAULT '' NOT NULL,
+  playedat VARCHAR(55) DEFAULT '' NOT NULL,
+  bwidthus VARCHAR(55) DEFAULT '' NOT NULL,
+  totbwidthus VARCHAR(255) DEFAULT '' NOT NULL,
+  date DATE DEFAULT '1970-01-01' NOT NULL,
+  time TIME DEFAULT '00:00:00' NOT NULL,
   PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Dumping data for table 'scastd_xml'
