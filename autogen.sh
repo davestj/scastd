02d42baab8c2b172f664de909db687b8a52a12b6
David Andrew St John <davestj@gmail.com>
2025-08-10 14:19:22 -0700

    Detect OS and include Homebrew paths

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,18 @@ srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
 cd "$srcdir"
 
+OS=$(uname -s)
+case "$OS" in
+  Darwin)
+    export PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig:/opt/homebrew/share/pkgconfig:${PKG_CONFIG_PATH}"
+    export CPPFLAGS="-I/opt/homebrew/include ${CPPFLAGS}"
+    export LDFLAGS="-L/opt/homebrew/lib ${LDFLAGS}"
+    ;;
+  Linux)
+    export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+    ;;
+esac
+
 echo "Generating configuration files for $package, please wait...."
 autoreconf -I m4 --install --force --verbose
 
